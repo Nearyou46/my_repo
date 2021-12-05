e5f63c27e95cbd7dc311159d63698b34acbf2b6d
root <root@localhost.localdomain>
2021-12-05 13:56:21 -0500

    new version

--- docker.sh
+++ docker.sh
@@ -1,4 +1,5 @@
 #!/bin/bash 
+echo "DBPASS = ${dbpass}" > /tmp/dbpass
 sudo apt update -y 
 sudo apt install apt-transport-https ca-certificates curl software-properties-common -y 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - 
@@ -6,12 +7,17 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 sudo apt update -y 
 apt-cache policy docker-ce 
 sudo apt install docker-ce -y 
+sudo groupadd docker
+sudo usermod -aG docker borys
 sudo mkdir /var/jenkins_home/
 sudo chmod 777 /var/jenkins_home/
 sudo docker pull jenkins/jenkins
 sudo docker run --rm --name jenkins -d -p 8282:8080 -v /var/jenkins_home:/var/jenkins_home/ jenkins/jenkins
 sudo docker volume create db
 sudo docker pull us.gcr.io/gcp101233-lv61301devops/646/postgres_lv646:latest
-sudo docker run --name db -d -e POSTGRES_PASSWORD=password -p 5432:5432 -v db:/var/lib/postgresql/data/pgdata us.gcr.io/gcp101233-lv61301devops/646/postgres_lv646:latest
+sudo docker run --rm --name db -d -e POSTGRES_PASSWORD=${dbpass} -p 5432:5432 -v db:/var/lib/postgresql/data/ us.gcr.io/gcp101233-lv61301devops/646/postgres_lv646:latest
 sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+sleep 10
+sudo docker run --rm --name db -d -e POSTGRES_PASSWORD=${dbpass} -p 5432:5432 -v db:/var/lib/postgresql/data/ us.gcr.io/gcp101233-lv61301devops/646/postgres_lv646:latest
+
--- gcp.tf
+++ gcp.tf
@@ -1,8 +1,7 @@
-##################
 provider "google" {
- credentials = file("credentials.json")
- project     = "gcp101233-lv61301devops"
- region      = "us-central1"
+  credentials = file("credentials.json")
+  project     = "gcp101233-lv61301devops"
+  region      = "us-central1"
 }
 ####################################
 resource "google_compute_address" "extipmain" {
@@ -14,40 +13,33 @@ resource "google_compute_instance" "default10" {
   machine_type = "e2-standard-4"
   zone         = "us-central1-a"
 
-  //user_data    = ("docker.sh")
-  metadata_startup_script = file("${path.module}/docker.sh")
-  tags = ["foo", "bar"]
+  metadata_startup_script = templatefile("${path.module}/docker.sh", {
+  dbpass = data.google_secret_manager_secret_version.postgres.secret_data
+  })
+
+  tags                    = ["foo", "bar"]
 
   boot_disk {
     initialize_params {
       image = "ubuntu-os-cloud/ubuntu-2004-lts"
     }
   }
-
-  // Local SSD disk
-  //scratch_disk {
-    //interface = "SCSI"
-  //}
-
-  network_interface {
+  
+   network_interface {
     network = "default"
-		    access_config {
-    nat_ip = "${google_compute_address.extipmain.address}"
+    access_config {
+      nat_ip = google_compute_address.extipmain.address
     }
   }
   metadata = {
     foo = "bar"
   }
+
+data "google_secret_manager_secret_version" "postgres" { 
+   secret = "postgres"
 }
 
-
-
-
-
-
-
-
-###########################################
+}
 
 ####################################
 resource "google_compute_address" "extipslave" {
@@ -57,8 +49,16 @@ resource "google_compute_instance" "default11" {
   name         = "slavemachine"
   machine_type = "e2-standard-2"
   zone         = "us-central1-a"
-  //user_data    = file("docker.sh")
-  metadata_startup_script = file("${path.module}/dockerr.sh")
+  
+  metadata_startup_script = templatefile("${path.module}/dockerr.sh", {
+  dbpass          = data.google_secret_manager_secret_version.postgres.secret_data
+  MY_PASSWORD     = data.google_secret_manager_secret_version.MY_PASSWORD.secret_data
+  DATASOURCE_USER = data.google_secret_manager_secret_version.DATASOURCE_USER.secret_data
+  DATASOURCE_URL = data.google_secret_manager_secret_version.DATASOURCE_URL.secret_data
+  JWT_SECRET      = data.google_secret_manager_secret_version.JWT_SECRET.secret_data
+  })
+  
+  
   tags = ["foo", "bar"]
   boot_disk {
     initialize_params {
@@ -68,11 +68,29 @@ resource "google_compute_instance" "default11" {
 
   network_interface {
     network = "default"
-			access_config {
+                        access_config {
     nat_ip = "${google_compute_address.extipslave.address}"
     }
   }
   metadata = {
     foo = "bar"
   }
+
+data "google_secret_manager_secret_version" "postgres" {
+  secret = "postgres"
 }
+data "google_secret_manager_secret_version" "MY_PASSWORD" {
+  secret = "MY_PASSWORD"
+}
+data "google_secret_manager_secret_version" "JWT_SECRET" {
+  secret = "JWT_SECRET"
+}
+data "google_secret_manager_secret_version" "DATASOURCE_USER" {
+  secret = "DATASOURCE_USER"
+}
+data "google_secret_manager_secret_version" "DATASOURCE_URL" {
+  secret = "DATASOURCE_URL"
+}
+
+}
+
